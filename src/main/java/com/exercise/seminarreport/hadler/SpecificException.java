caabe65e581d8087f0d672529a66bf1b2a0698a0
AzureAD\Employee0724 <sornpata@wisesoft.co.th>
2024-09-03 07:54:55 +0700

    exception Controller Advice

---
+++ src/main/java/com/exercise/seminarreport/hadler/SpecificException.java
@@ -0,0 +1,7 @@
+package com.exercise.seminarreport.hadler;
+
+public class SpecificException extends RuntimeException {
+    public SpecificException(String message) {
+        super(message);
+    }
+}
